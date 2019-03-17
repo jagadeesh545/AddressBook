d384177c4be72cb50c91ddfc5fa30e9a03b40597
Jagadeesh Madagundi <jagadeesh545@gmail.com>
2019-03-17 16:08:22 -0400

    add login_required restriction

--- contact/views.py
+++ contact/views.py
@@ -4,9 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views import generic
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
-#@login_required
+@method_decorator(login_required, name='dispatch')
 class ContactList(generic.ListView):
     model = Contact
     template_name = 'contact/contact_list.html'
+
+    def dispatch(self, *args, **kwargs):
+        return super(generic.ListView, self).dispatch(*args, **kwargs)
