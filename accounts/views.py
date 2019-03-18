f554c0aaef06baa65d2fa56047b75333f1a5bcae
Jagadeesh Madagundi <jagadeesh545@gmail.com>
2019-03-18 01:28:05 -0400

    UI Improvements and Documentation.

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ from django.views import generic
 
 
 class SignUp(generic.CreateView):
+    """ Sign-up class to allow new users to create an account in the system"""
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class ContactSearchForm(forms.Form):
+    """Simple text field form to allow users to search for contacts."""
     search_name = forms.CharField(max_length=10)
